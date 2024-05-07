0773aa1579ef15a1b392ac55f64f4e08fc089076
sirdazzle <fkdlffkr1742@naver.com>
2024-05-08 01:51:03 +0900

    데이터베이스 연결
    - 버튼 초기 설정 오류 해결
    - 데이터베이스에 저장된 디바이스는 이제 앱을 종료했다가 다시 실행했을 때 정상적으로 보입니다.
    - 디바이스의 이름을 변경한 후 앱을 종료했다가 실행해도 정상적으로 변경한 디바이스 이름이 출력됩니다.
    
    x) 디바이스를 삭제해도 새로고침 전까지는 잔상이 남아있습니다. (단, 데이터베이스에선 정상적으로 즉각 삭제됩니다.)
    x) 디바이스가 1개일 시, 앱 중앙에 정렬하는 UI 수정은 아직 이루어지지 않았습니다.

--- lib/main.dart
+++ lib/main.dart
@@ -47,41 +47,27 @@ class _AddDevice extends State<AddButtons> {
   List<ButtonData> buttons = [];
 
   @override
+  void initState() {
+    super.initState();
+    _fetchButtonsFromFirestore();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50.0),
-                scrollDirection: Axis.horizontal,
-                itemCount: buttons.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(horizontal: 30.0),
-                    child: InkWell(
-                      onTap: () => _showEditDialog(index),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            height: 150,
-                            width: 150,
-                            child: Image.asset('images/devices.png', fit: BoxFit.cover),
-                          ),
-                          Text(buttons[index].name,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),]
+        children: [
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50.0),
+              scrollDirection: Axis.horizontal,
+              itemCount: buttons.length,
+              itemBuilder: (context, index) {
+                return _buildButton(index);
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _addButton,
@@ -94,14 +80,58 @@ class _AddDevice extends State<AddButtons> {
     );
   }
 
+  @override
+  Widget _buildButton(int index) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30.0),
+      child: InkWell(
+        onTap: () => _showEditDialog(index),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 150,
+              width: 150,
+              child: Image.asset('images/devices.png', fit: BoxFit.cover),
+            ),
+            Text(
+              buttons[index].dvName, // dvName을 표시
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _fetchButtonsFromFirestore() async {
+    QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('device').get();
+    setState(() {
+      buttons = snapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        return ButtonData(
+          data['name'],
+          data['isActive1'] == '1',
+          data['isActive2'] == '1',
+          dvName: data['dvName'], // Include dvName in the data retrieval
+        );
+      }).toList();
+    });
+  }
+
   void _addButton() {
     setState(() {
-      buttons.add(ButtonData('DEVICE${buttons.length + 1}', true, true));
+      buttons.add(ButtonData('DEVICE${buttons.length + 1}', true, true, dvName: 'DEVICE${buttons.length + 1}'));
+      // Provide dvName as 'DEVICE${buttons.length + 1}'
     });
   }
 
   void _showEditDialog(int index) {
-    TextEditingController nameController = TextEditingController(text: buttons[index].name);
+    TextEditingController nameController = TextEditingController(text: buttons[index].dvName); // dvName 사용
     bool tempIsActive1 = buttons[index].isActive1;
     bool tempIsActive2 = buttons[index].isActive2;
     bool originalIsActive1 = tempIsActive1; // 초기 활성화 스위치 상태 저장
@@ -113,97 +143,98 @@ class _AddDevice extends State<AddButtons> {
         return StatefulBuilder(
           builder: (BuildContext context, StateSetter setState) {
             return AlertDialog(
-                backgroundColor: Colors.white,
-                contentTextStyle: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 17
-                ),
-                content: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: nameController,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.black54),
-                      decoration: InputDecoration(
-                          labelStyle: TextStyle(color: Colors.white),
-                          enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.transparent)
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.transparent)
-                          )
+              backgroundColor: Colors.white,
+              contentTextStyle: TextStyle(
+                color: Colors.black54,
+                fontSize: 17,
+              ),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                    controller: nameController,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black54),
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.white),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
                       ),
-                      onChanged: (text) {
-                        setState(() {
-                          buttons[index].name = text;
-                        });
-                      },
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
                     ),
-                    Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('활성화  '),
-                            Switch(
-                              value: tempIsActive1,
-                              onChanged: (bool value) {
-                                setState(() {
-                                  tempIsActive1 = value;
-                                });
-                              },
-                              activeTrackColor: Colors.black54,
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(' 알  림  '),
-                            Switch(
-                              value: tempIsActive2,
-                              onChanged: (bool value) {
-                                setState(() {
-                                  tempIsActive2 = value;
-                                });
-                              },
-                              activeTrackColor: Colors.black54,
-                            ),
-                          ],
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                actions: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.delete, color: Colors.black45),
-                        onPressed: () {
-                          setState(() {
-                            buttons.removeAt(index);
-                          });
-                          Navigator.of(context).pop();
-                        },
+                    onChanged: (text) {
+                      setState(() {
+                        buttons[index].dvName = text; // dvName을 변경한 값으로 설정
+                      });
+                    },
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('활성화  '),
+                          Switch(
+                            value: tempIsActive1,
+                            onChanged: (bool value) {
+                              setState(() {
+                                tempIsActive1 = value;
+                              });
+                            },
+                            activeTrackColor: Colors.black54,
+                          ),
+                        ],
                       ),
-                      Spacer(),
-                      TextButton(
-                        onPressed: () {
-                          _saveDeviceStateToFirestore(
-                              buttons[index],
-                              tempIsActive1 ? true : false,
-                              tempIsActive2 ? true : false
-                          );
-                          Navigator.of(context).pop();
-                        },
-                        child: Text('저장', style: TextStyle(color: Colors.black54)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(' 알  림  '),
+                          Switch(
+                            value: tempIsActive2,
+                            onChanged: (bool value) {
+                              setState(() {
+                                tempIsActive2 = value;
+                              });
+                            },
+                            activeTrackColor: Colors.black54,
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ]
+                ],
+              ),
+              actions: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.delete, color: Colors.black45),
+                      onPressed: () {
+                        _deleteDeviceFromFirestore(buttons[index]);
+                        setState(() {
+                          buttons.removeAt(index);
+                        });
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    Spacer(),
+                    TextButton(
+                      onPressed: () {
+                        _saveDeviceStateToFirestore(
+                          buttons[index],
+                          tempIsActive1 ? true : false,
+                          tempIsActive2 ? true : false,
+                        );
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('저장', style: TextStyle(color: Colors.black54)),
+                    ),
+                  ],
+                ),
+              ],
             );
           },
         );
@@ -211,29 +242,53 @@ class _AddDevice extends State<AddButtons> {
     ).then((_) {
       // 팝업이 닫힌 후에 기존 데이터를 다시 불러와서 스위치의 초기 상태를 설정
       setState(() {
-        buttons[index].isActive1 = originalIsActive1;
-        buttons[index].isActive2 = originalIsActive2;
+        // 데이터베이스에서 가져온 값에 따라 스위치의 초기 상태 설정
+        buttons[index].isActive1 = tempIsActive1;
+        buttons[index].isActive2 = tempIsActive2;
       });
+    }).then((_) async {
+      // Firestore에서 isActive1, isActive2 값 가져오기
+      DocumentSnapshot snapshot =
+      await FirebaseFirestore.instance.collection('device').doc(buttons[index].dvName).get(); // dvName 사용
+      if (snapshot.exists) {
+        // 데이터베이스에서 isActive1, isActive2 값 가져오기
+        Map<String, dynamic>? data = snapshot.data() as Map<String, dynamic>?; // 캐스팅
+        if (data != null) {
+          // null 체크
+          String? isActive1DB = data['isActive1'] as String?; // 캐스팅
+          String? isActive2DB = data['isActive2'] as String?; // 캐스팅
+          if (isActive1DB != null && isActive2DB != null) {
+            // null 체크
+            setState(() {
+              // isActive1, isActive2 값에 따라 스위치의 초기 상태 설정
+              buttons[index].isActive1 = isActive1DB == '1'; // '1'이면 true, 그 외에는 false로 설정
+              buttons[index].isActive2 = isActive2DB == '1'; // '1'이면 true, 그 외에는 false로 설정
+            });
+          }
+        }
+      }
     });
   }
-
-
-
-
-
+  void _deleteDeviceFromFirestore(ButtonData button) {
+    // Firestore에서 해당 문서 삭제
+    FirebaseFirestore.instance.collection('device').doc(button.name).delete();
+  }
   void _saveDeviceStateToFirestore(ButtonData button, bool isActive1, bool isActive2) {
     // Firestore에 연결
     FirebaseFirestore.instance.collection('device').doc(button.name).set({
+      'name': button.name,
       'isActive1': isActive1 ? '1' : '0', // 활성화 스위치 상태 저장
       'isActive2': isActive2 ? '1' : '0', // 알림 스위치 상태 저장
+      'dvName': button.dvName,
     });
   }
 }
 
 class ButtonData {
   String name;
+  String dvName; // 디바이스 이름을 저장할 필드 추가
   bool isActive1;
   bool isActive2;
 
-  ButtonData(this.name, this.isActive1, this.isActive2);
+  ButtonData(this.name, this.isActive1, this.isActive2, {required this.dvName}); // 초기화 시 디바이스 이름과 동일하게 설정
 }
